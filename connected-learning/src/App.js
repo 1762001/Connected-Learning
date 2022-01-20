716ade65f29ab7a4355bd4522f49a24dfa205ecf
Rudra1125 <shankarrudra36@gmail.com>
2022-01-20 21:51:07 +0530

    Added component folder and navbar

--- connected-learning/src/App.js
+++ connected-learning/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="app">
-      <h1>HII CODERS</h1>
-    
+      <Navbar/>
     </div>
   );
 }
