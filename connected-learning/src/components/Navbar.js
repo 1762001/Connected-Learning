73b4f87951f40813e9877c48e87607f3806b63d3
johnwick1793 <88131089+johnwick1793@users.noreply.github.com>
2022-01-21 18:49:29 +0530

    changes in nav sizes

--- connected-learning/src/components/Navbar.js
+++ connected-learning/src/components/Navbar.js
@@ -15,9 +15,11 @@ class Navbar extends Component {
             <li><a href="/"><span style={{marginRight: "5px"}}><i className="fas fa-book-reader"></i></span><span>Encyclopedia</span>
             </a></li>
           
-            <li><a href="/"><span style={{marginRight: "5px"}}><i className="far fa-id-card"></i></span><span>Profile Enhancer</span></a></li>
-            <li><a href="/"><span style={{marginRight: "5px"}}><i className="fas fa-comment"></i></span><span>Chat</span></a></li>
-            <li><span><a href="/"><span style={{backgroundColor: "lightslategray", borderRadius: "50%" , marginRight: "5px"}}><i className="fas fa-user-circle"></i></span><span>sign out</span></a></span></li>
+          <li><a href="/"><span style={{marginRight: "5px"}}><i className="far fa-id-card"></i></span><span>Profile Enhancer</span></a></li>
+            <li><a href="/"><span style={{marginRight: "5px"}}><i className="fas fa-comment"></i></span><span >Chat</span></a></li>
+            <li><a href="/"><span style={{marginRight: "5px"}}><i className="fas fa-envelope-open-text"></i></span></a></li>
+            <li><span><a href="/"><span style={{backgroundColor: "lightslategray", borderRadius: "50%" , marginRight: "5px"}}><i className="fas fa-user-circle"></i></span></a></span></li>
+
             <li><form className="d-flex">
               <input className="form-control searchbox me-2 p-1" type="search" placeholder="Search" aria-label="Search"/>
               
