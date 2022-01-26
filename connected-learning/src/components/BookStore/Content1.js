92315c425f9bb943b4f972d5b902362a38783579
Saikat-Chowdhury90 <sonusaikat09@gmail.com>
2022-01-27 00:34:54 +0530

    changes in margin

--- connected-learning/src/components/BookStore/Content1.js
+++ connected-learning/src/components/BookStore/Content1.js
@@ -4,7 +4,7 @@ import './Content1.css'
 function Content1() {
   return (
     <div className='content'>
-        <h1 className='mt-2 center'>BOOKS</h1>
+        <h1 className='mt-2 center mb-3'>BOOKS</h1>
         <div className='Book_Display'>
               <Books/>
               <Books/>
