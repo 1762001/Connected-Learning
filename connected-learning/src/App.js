1fcbe3f560b06cd67b50bbd621b2fb671ae5c10e
Saikat-Chowdhury90 <sonusaikat09@gmail.com>
2022-01-27 00:32:32 +0530

    organised all the changes

--- connected-learning/src/App.js
+++ connected-learning/src/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
     <div className="app">
       <Navbar/>
-      {/* <BookStore/> */}
-      <Content/>
+      <BookStore/>
+      {/* <Content/> */}
     </div>
   );
 }
--- connected-learning/src/BookStore.js
+++ connected-learning/src/BookStore.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Banner from './components/Banner';
-import Content1 from './components/Content1'
+import Content1 from './components/BookStore/Content1'
 import Sidebar from './components/Sidebar'
 
 function BookStore() {
